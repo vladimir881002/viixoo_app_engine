e56d855ad9e2e7c776d6df289633017b387351a7
Yaricel <yaricelgv90@gmail.com>
2025-03-26 12:06:22 -0400

    Corregir errores de pre-commit

--- viixoo_app_engine/viixoo_backend_apps/mrp/services/security.py
+++ viixoo_app_engine/viixoo_backend_apps/mrp/services/security.py
@@ -1,3 +1,5 @@
+"""This module contains functions for managing application security, including the creation and verification of JWT tokens."""
+
 from datetime import datetime, timedelta, timezone
 from typing import Any
 import secrets
@@ -14,6 +16,13 @@ SECRET_KEY: str = secrets.token_urlsafe(32)
 
 
 def create_access_token(subject: str | Any, expires_delta: timedelta) -> str:
+    """
+    Creates a JWT access token for the given subject.
+
+    :param subject: The subject of the token (e.g., a user ID).
+    :param expires_delta: The token's expiration time.
+    :return: The encoded JWT token.
+    """
     expire = datetime.now(timezone.utc) + expires_delta
     to_encode = {"exp": expire, "sub": str(subject)}
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
@@ -21,8 +30,21 @@ def create_access_token(subject: str | Any, expires_delta: timedelta) -> str:
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """
+    Verify if a plaintext password matches an encrypted password.
+
+    :param plain_password: Plaintext password.
+    :param hashed_password: Encrypted password.
+    :return: True if a match, False otherwise.
+    """
     return pwd_context.verify(plain_password, hashed_password)
 
 
 def get_password_hash(password: str) -> str:
+    """
+    Encrypts a password using the configured encryption context.
+
+    :param password: Password to encrypt.
+    :return: Encrypted password.
+    """
     return pwd_context.hash(password)
