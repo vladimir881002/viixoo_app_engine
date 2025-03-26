ef0061e11d303903c653def5bbe3c85574879f0a
Yaricel <yaricelgv90@gmail.com>
2025-03-26 12:51:26 -0400

    Corregir errores de pre-commit

--- viixoo_app_engine/viixoo_frontend_apps/vite.config.ts
+++ viixoo_app_engine/viixoo_frontend_apps/vite.config.ts
@@ -1,7 +1,7 @@
-import path from "node:path";
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
-import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import path from "node:path"
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin"
+import react from "@vitejs/plugin-react-swc"
+import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,4 +11,10 @@ export default defineConfig({
     },
   },
   plugins: [react(), TanStackRouterVite()],
-});
+  build: {
+    sourcemap: true,
+  },
+  css: {
+    devSourcemap: true,
+  },
+})
